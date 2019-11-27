cba21a4650c49a8f97f47a8df27773dffcdd7a54
Tyler <cronistertyler@gmail.com>
2019-11-27 14:52:59 -0600

    Creation of the pwd generator

---
+++ pwd_gen.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+# collecting the max length of the password
+max = raw_input("Max length? ")
+pwd = ''
+special = ['!', '@', '#', '$', '%', '&']
+random.seed(int(round(time.time() * 1000)))
+
+for count in range(int(max)):
+    type = random.randint(1,3)
+    if type == 1:
+	pwd += chr(random.randint(65,90))
+    elif type == 2:
+	pwd += chr(random.randint(97,122))
+    else:
+	pwd += random.choice(special)
+
+print(pwd)   
