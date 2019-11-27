1a6bfbe0ff2f64f59f0e8fe208999883bdab3ff7
tcronis <46980360+tcronis@users.noreply.github.com>
2019-11-27 14:59:30 -0600

    tab change

--- pwd_gen.py
+++ pwd_gen.py
@@ -10,10 +10,9 @@ random.seed(int(round(time.time() * 1000)))
 for count in range(int(max)):
     type = random.randint(1,3)
     if type == 1:
-	pwd += chr(random.randint(65,90))
+	    pwd += chr(random.randint(65,90))
     elif type == 2:
-	pwd += chr(random.randint(97,122))
+	    pwd += chr(random.randint(97,122))
     else:
-	pwd += random.choice(special)
-
+	    pwd += random.choice(special)
 print(pwd)   
